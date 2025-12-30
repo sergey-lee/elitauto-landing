d7d8798bc02de0a3d4025d493f63ef0aaa8f05df
sergey-lee <guagetru.bla@gmail.com>
2025-12-30 21:15:41 +0900

    - lexus

--- src/app/components/PopularCars.tsx
+++ src/app/components/PopularCars.tsx
@@ -34,17 +34,17 @@ export function PopularCars() {
     },
     {
       brand: 'Hyundai',
-      model: 'Palisade',
-      image: 'https://github.com/sergey-lee/elitauto-landing/blob/main/src/assets/palisade.jpg?raw=true',
-      description: 'Современный семейный SUV',
-      price: 'от $30,000'
+      model: 'Sonata',
+      image: 'https://images.unsplash.com/photo-1726038021851-9dc24938b376?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxoeXVuZGFpJTIwc29uYXRhfGVufDF8fHx8MTc2Njg5OTUyM3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      description: 'Современный семейный седан',
+      price: 'от $5,000'
     },
     {
-      brand: 'Kia',
-      model: 'Sportage',
-      image: 'https://github.com/sergey-lee/elitauto-landing/blob/main/src/assets/sportage.jpg?raw=true',
-      description: 'Практичный городской SUV',
-      price: 'от $27,000'
+      brand: 'Hyundai',
+      model: 'Palisade',
+      image: 'https://github.com/sergey-lee/elitauto-landing/blob/main/src/assets/palisade.jpg?raw=true',
+      description: 'Премиальный семейный SUV',
+      price: 'от $30,000'
     }
   ];
 
